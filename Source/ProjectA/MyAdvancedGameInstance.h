0e5f838b3420a604e36995809e9daa9de802fae1
IlsanGaming <whrkdgus34@gmail.com>
2025-06-13 06:52:56 +0900

    프로젝트 파일 업로드 4

---
+++ Source/ProjectA/MyAdvancedGameInstance.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AdvancedFriendsGameInstance.h"
+#include "MyAdvancedGameInstance.generated.h"
+
+UCLASS()
+class PROJECTA_API UMyAdvancedGameInstance : public UAdvancedFriendsGameInstance
+{
+    GENERATED_BODY()
+};
